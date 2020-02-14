8f30f56ce52dd755480d10368d94a486d3eec88f
duanmuchaoqun <2947824360@qq.com>
2020-02-14 19:05:36 +0800

    Initial commit

---
+++ src/com/atguigu/java/Hello.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+/**
+ * @author duanmuchaoqun
+ * @date 2020/2/14 - 15:52
+ */
+public class Hello {
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+    private static final int INITIAL_SIZE = 10;
+
+    private static final int INITIAL_TOTAL = 100;
+
+    public static void main(String[] args) {
+        System.out.println("hello world !!!");
+
+        System.out.println();
+        ArrayList list = new ArrayList();
+
+
+    }
+
+    public HelloWorld() {
+    }
+}
